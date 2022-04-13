17dd2386108603a46f3c6953fdea0c8f44014e60
mhmunter <mark.munter1@gmail.com>
2022-04-13 09:56:37 -0500

    add heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 const { animals } = require('./data/animals');
 
 // function filterByQuery(query, animalsArray) {
